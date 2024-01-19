b9e94d1717300974f7ed8344fe0a36e946bf74ab
ammar <ammarkhan575@gmail.com>
2024-01-20 02:01:10 +0530

    footer completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
         <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
-            <h3 className="font-bold text-2xl">Wheelers.</h3>
+        <h3 className="font-bold text-2xl">Wheelers.</h3>
             <CustomButton 
                 title="Sign In"
                 btnType="button"
