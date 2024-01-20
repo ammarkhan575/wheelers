71ba38335e224e1f9dcb2f0c63fb2df155e285c3
ammar <ammarkhan575@gmail.com>
2024-01-20 14:49:25 +0530

    added selected option in searchManufacturer with tick mark

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -72,7 +72,26 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                     }
                     value={item}
                   >
-                    {item}
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            ✅
+                          </span>
+                        ) : null}
+                      </>
+                    )}
                   </Combobox.Option>
                 ))
               )}
